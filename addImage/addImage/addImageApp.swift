35842f71d2ed1049abd1cbe053b9c00e9b5a51b3
0429oonishi <oonishireon110@gmail.com>
2021-06-01 12:12:46 +0900

    addImage first commit

---
+++ addImage/addImage/addImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  addImageApp.swift
+//  addImage
+//
+//  Created by 大西玲音 on 2021/06/01.
+//
+
+import SwiftUI
+
+@main
+struct addImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
