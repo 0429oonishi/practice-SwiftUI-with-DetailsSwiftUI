74b4ea2d6372c51f2e7d82bad674cfaa1d1fe3eb
0429oonishi <oonishireon110@gmail.com>
2021-06-03 14:04:49 +0900

    List練習

---
+++ ContentList/ContentList/ContentListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentListApp.swift
+//  ContentList
+//
+//  Created by 大西玲音 on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct ContentListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentList/ContentList/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  ContentList
+//
+//  Created by 大西玲音 on 2021/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("text1")
+                Text("text2")
+                Photo1().frame(width: 400, height: 100)
+                Text("text3")
+                Text("text4")
+                Photo2().frame(width: 200, height: 100)
+                Text("text5")
+            }
+            .navigationTitle("ポケモン")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView2: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello World")
+            Divider()
+            Photo1().frame(width: 400, height: 100)
+            Divider()
+            Divider()
+            Divider()
+            Divider()
+            Photo2().frame(width: 400, height: 100)
+            Divider()
+            Spacer()
+        }
+        .padding(.all, 10)
+    }
+}
+
+struct Photo1: View {
+    var body: some View {
+        HStack {
+            Image("a")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("みゅー")
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct Photo2: View {
+    var body: some View {
+        HStack {
+            Image("b")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("ピカチュー")
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView2()
+    }
+}
