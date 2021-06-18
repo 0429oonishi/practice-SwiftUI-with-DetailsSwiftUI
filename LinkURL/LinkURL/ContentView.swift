c4f43579c342bbc21f3194a0656d5cae53c3acbb
0429oonishi <oonishireon110@gmail.com>
2021-06-18 21:49:19 +0900

    実装

--- LinkURL/LinkURL/ContentView.swift
+++ LinkURL/LinkURL/ContentView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let webList = [
+        WebData(name: "ピカチュウ",
+                url: "https://www.apple.com/",
+                favicon: "ピカチュウ"),
+        WebData(name: "イーブイ",
+                url: "https://github.com/0429oonishi",
+                favicon: "イーブイ"),
+        WebData(name: "ヒトカゲ",
+                url: "https://qiita.com/REON",
+                favicon: "ヒトカゲ")
+        
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(webList) { item in
+                Image(item.favicon)
+                    .resizable()
+                    .frame(width: 100,
+                           height: 100)
+                Link(item.name,
+                     destination: URL(string: item.url)!)
+            }
+            .navigationBarTitle("Webリスト")
+        }
     }
+}
+
+struct WebData: Identifiable {
+    var id = UUID()
+    var name: String
+    var url: String
+    var favicon: String
 }
 
 struct ContentView_Previews: PreviewProvider {
