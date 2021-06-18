ff50b6411d61020b95f35f7e45055d1aac23aafd
0429oonishi <oonishireon110@gmail.com>
2021-06-18 21:25:28 +0900

    init commit

---
+++ LinkURL/LinkURL/LinkURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkURLApp.swift
+//  LinkURL
+//
+//  Created by 大西玲音 on 2021/06/18.
+//
+
+import SwiftUI
+
+@main
+struct LinkURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
