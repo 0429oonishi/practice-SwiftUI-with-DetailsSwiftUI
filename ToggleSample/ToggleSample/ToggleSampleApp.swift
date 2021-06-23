8d23380ecc03b5d1db63a323994d3862263bea3e
0429oonishi <oonishireon110@gmail.com>
2021-06-23 13:17:19 +0900

    first commit

---
+++ ToggleSample/ToggleSample/ToggleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleSampleApp.swift
+//  ToggleSample
+//
+//  Created by 大西玲音 on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToggleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
