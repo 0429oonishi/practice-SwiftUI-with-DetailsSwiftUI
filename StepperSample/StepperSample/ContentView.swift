7e30d43e8b458d2718c300fbccd42092418c2813
0429oonishi <oonishireon110@gmail.com>
2021-06-24 14:28:17 +0900

    StepperSample first commit

---
+++ StepperSample/StepperSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StepperSample
+//
+//  Created by 大西玲音 on 2021/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StepperSample/StepperSample/StepperSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperSampleApp.swift
+//  StepperSample
+//
+//  Created by 大西玲音 on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct StepperSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
