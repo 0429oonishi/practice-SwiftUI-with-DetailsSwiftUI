ea5cee9a51c630b945312450f6be03589ec9de0a
0429oonishi <oonishireon110@gmail.com>
2021-06-27 20:27:10 +0900

    a

---
+++ SliderSample/SliderSample/SliderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSampleApp.swift
+//  SliderSample
+//
+//  Created by 大西玲音 on 2021/06/27.
+//
+
+import SwiftUI
+
+@main
+struct SliderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
