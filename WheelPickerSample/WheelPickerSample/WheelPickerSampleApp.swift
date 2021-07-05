1ac4b2cc6fcb42fee9792e3f5d4b5694516b66dc
0429oonishi <oonishireon110@gmail.com>
2021-07-05 12:19:54 +0900

    weelPicker first commit

---
+++ WheelPickerSample/WheelPickerSample/WheelPickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelPickerSampleApp.swift
+//  WheelPickerSample
+//
+//  Created by 大西玲音 on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct WheelPickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
