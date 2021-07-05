99d8db2b29d5afc4a007cbe0dbc82f534e9e33c6
0429oonishi <oonishireon110@gmail.com>
2021-07-05 12:33:25 +0900

    実装

--- WheelPickerSample/WheelPickerSample/ContentView.swift
+++ WheelPickerSample/WheelPickerSample/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedColor = 0
+    let colorViews = [Color.red, Color.green, Color.blue]
+    let colorNames = ["Red", "Green", "Blue"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker(selection: $selectedColor,
+                   label: Text("Color")) {
+                Text("Red").tag(0)
+                Text("Green").tag(1)
+                Text("Blue").tag(2)
+            }
+            HStack {
+                colorViews[selectedColor]
+                    .frame(width: 50, height: 100)
+                Text("value: \(selectedColor)")
+                    .frame(width: 60)
+                Text("color: \(colorNames[selectedColor])")
+                    .frame(width: 100)
+                
+            }
+        }
+        .padding()
     }
 }
 
