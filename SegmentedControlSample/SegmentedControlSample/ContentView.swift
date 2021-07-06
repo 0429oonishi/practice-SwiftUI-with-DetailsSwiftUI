6aa79a73fbbf46f26c6d08cb42deee06c101b913
0429oonishi <oonishireon110@gmail.com>
2021-07-06 10:51:06 +0900

    SegmentedControlSample実装

--- SegmentedControlSample/SegmentedControlSample/ContentView.swift
+++ SegmentedControlSample/SegmentedControlSample/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedColorNumber = 0
+    let colorViews: [Color] = [.red, .green, .blue]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker(selection: $selectedColorNumber,
+                   label: Text("Color")) {
+                Text("Red").tag(0)
+                Text("Green").tag(1)
+                Text("Blue").tag(2)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            symbolImage(num: selectedColorNumber)
+                .resizable()
+                .foregroundColor(colorViews[selectedColorNumber])
+                .frame(width: 100, height: 100)
+                .padding()
+        }
+    }
+    
+    func symbolImage(num: Int) -> Image {
+        switch num {
+            case 0: return Image(systemName: "r.circle")
+            case 1: return Image(systemName: "g.circle")
+            case 2: return Image(systemName: "b.circle")
+            default: return Image(systemName: "r.circle")
+        }
     }
 }
 
