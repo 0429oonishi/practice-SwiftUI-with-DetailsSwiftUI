fc6763e2e6d3ad11b752937581b11c1da6187999
0429oonishi <oonishireon110@gmail.com>
2021-07-06 10:44:26 +0900

    segmentedControl first commit

---
+++ SegmentedControlSample/SegmentedControlSample/SegmentedControlSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlSampleApp.swift
+//  SegmentedControlSample
+//
+//  Created by 大西玲音 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
