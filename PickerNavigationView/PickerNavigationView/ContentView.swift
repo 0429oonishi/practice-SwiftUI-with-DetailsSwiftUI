7c80347b5eb4db104e307137355d816e68d67f24
0429oonishi <oonishireon110@gmail.com>
2021-07-07 15:32:11 +0900

    PickerNavigationView実装

--- PickerNavigationView/PickerNavigationView/ContentView.swift
+++ PickerNavigationView/PickerNavigationView/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedSize = 2
+    @State var selectedColor = 0
+    let sizes = ["XS", "S", "M", "L", "XL"]
+    let colors = ["Red", "Green", "Blue"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("サイズ").font(.headline),
+                        footer: Text("UIサイズの少し大きめです").padding(.bottom, 20)) {
+                    Picker(selection: $selectedSize, label: Text("Size")) {
+                        ForEach(0..<sizes.count) { index in
+                            Text(sizes[index])
+                        }
+                    }
+                    Text("選んだサイズ: \(sizes[selectedSize])")
+                }
+                
+                Section(header: Text("カラー").font(.headline)) {
+                    Picker(selection: $selectedColor, label: Text("Color")) {
+                        ForEach(0..<colors.count) { index in
+                            Text(colors[index])
+                        }
+                    }
+                    Text("選んだ色: \(colors[selectedColor])")
+                }
+            }
+            .navigationTitle(Text("サイズと色"))
+        }
     }
 }
 
