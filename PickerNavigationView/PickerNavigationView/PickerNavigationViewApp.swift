8ecd5b303d944c45fd474b83b8baae9eaf9d8b65
0429oonishi <oonishireon110@gmail.com>
2021-07-07 15:04:59 +0900

    PickerNavigationView first commit

---
+++ PickerNavigationView/PickerNavigationView/PickerNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerNavigationViewApp.swift
+//  PickerNavigationView
+//
+//  Created by 大西玲音 on 2021/07/07.
+//
+
+import SwiftUI
+
+@main
+struct PickerNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
