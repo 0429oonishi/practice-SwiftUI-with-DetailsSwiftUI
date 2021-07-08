f815c2914ffdf99e9cd9741480a07a84ba8e7177
0429oonishi <oonishireon110@gmail.com>
2021-07-08 17:14:05 +0900

    DateFormatter作成

--- DatePicker/DatePicker/ContentView.swift
+++ DatePicker/DatePicker/ContentView.swift
@@ -10,23 +10,32 @@ import SwiftUI
 struct ContentView: View {
     @State var theDate = Date()
     
-    var dateClosedRange: ClosedRange<Date> {
-        let min = Calendar.current.date(byAdding: .day,
-                                        value: -7,
-                                        to: Date())!
-        let max = Calendar.current.date(byAdding: .month,
-                                        value: 1,
-                                        to: Date())!
-        return min...max
+    var dateFormat: DateFormatter {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ja_JP")
+        df.dateStyle = .full
+        df.timeStyle = .short
+        return df
+    }
+    
+    var dateFormat2: DateFormatter {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ja_JP")
+        df.calendar = Calendar(identifier: .japanese)
+        df.dateFormat = "令和yy(YYYY)年M月dd日(E)HH時mm分"
+        return df
     }
     
     var body: some View {
+        VStack {
+            Text(dateFormat.string(from: theDate))
+            Text(dateFormat2.string(from: theDate))
         DatePicker(selection: $theDate,
-                   in: dateClosedRange,
-                   label: { Text("日時") })
+                   label: { EmptyView() })
             .environment(\.locale, Locale(identifier: "ja_JP"))
-            .frame(height: 50)
-        .padding(50)
+            .frame(width: 2000, height: 40)
+            .padding(50)
+        }
     }
 }
 
