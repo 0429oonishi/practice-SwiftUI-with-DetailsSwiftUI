9689f873856fca10b604d3a8ff580d3ae32d421b
0429oonishi <oonishireon110@gmail.com>
2021-07-08 16:48:53 +0900

    DatePicker first commit

---
+++ DatePicker/DatePicker/DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerApp.swift
+//  DatePicker
+//
+//  Created by 大西玲音 on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
