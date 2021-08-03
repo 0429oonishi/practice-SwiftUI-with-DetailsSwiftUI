3ed1f13416dabe0b37b3dbefde7df96361214d71
0429oonishi <oonishireon110@gmail.com>
2021-08-03 14:44:22 +0900

    TextFieldSample実装

--- TextFieldSample/TextFieldSample/ContentView.swift
+++ TextFieldSample/TextFieldSample/ContentView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: String = ""
+    private let tanka: Double = 250
+    private let tax: Double = 1.1
+    
     var body: some View {
-        TextField(<#T##titleKey: LocalizedStringKey##LocalizedStringKey#>, text: <#T##Binding<String>#>)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("個数: ")
+                    .padding(.horizontal)
+                TextField("0", text: $count)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(width: 100)
+            }
+            .font(.title)
+            .frame(width: 200)
+            Group {
+                if checkCount(min: 1, max: 10) {
+                    Text("\(price())円です")
+                        .font(.title)
+                } else {
+                    Text("個数は1〜10個を入れてください")
+                        .foregroundColor(.red)
+                        .font(.headline)
+                }
+            }
+            .frame(width: 300, height: 30)
+        }
+    }
+    
+    private func checkCount(min: Int, max: Int) -> Bool {
+        guard let num = Int(count) else { return false }
+        return (num >= min && num <= max)
+    }
+    
+    private func price() -> Int {
+        if let num = Double(count) {
+            let result = Int(tanka * num * tax)
+            return result
+        }
+        return -1
     }
 }
 
