701870fd0b043f33d0127a5e56581559bffabcea
0429oonishi <oonishireon110@gmail.com>
2021-08-03 13:59:00 +0900

    TextFieldSample first commit

---
+++ TextFieldSample/TextFieldSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TextFieldSample
+//
+//  Created by 大西玲音 on 2021/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TextField(<#T##titleKey: LocalizedStringKey##LocalizedStringKey#>, text: <#T##Binding<String>#>)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextFieldSample/TextFieldSample/TextFieldSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldSampleApp.swift
+//  TextFieldSample
+//
+//  Created by 大西玲音 on 2021/08/03.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
